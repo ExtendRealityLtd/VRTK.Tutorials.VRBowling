a0aa365a798736e3cf1c577d9e0d4d812ca13b1f
extendreality <hello@extendreality.io>
2021-02-13 12:30:43 +0000

    feat(scene): add check to see if pin has toppled over
    The new Pin script checks to see if a pin has been knocked over
    after it has been collided with. The check will run for a number of
    tries upon first collision to see if the pin has changed from its
    initial standing up rotation and if it has then the pin is hidden
    from the scene after a short amount of time.

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Zinnia.Extension;
+
+public class Pin : MonoBehaviour
+{
+    public float pinFallThreshold = 0.02f;
+    public float toppleLife = 3f;
+    public int tries = 10;
+
+    private Quaternion defaultRotation;
+    private int currentTries;
+
+    protected void Awake()
+    {
+        defaultRotation = transform.localRotation;
+    }
+
+    public void CheckTopple()
+    {
+        CancelToppleCheck();
+        CheckRotation();
+        InvokeRepeating("CheckRotation", 0f, 1f);
+    }
+
+    public void CancelToppleCheck()
+    {
+        currentTries = 0;
+        CancelInvoke("CheckRotation");
+        CancelInvoke("HidePin");
+    }
+
+    protected void CheckRotation()
+    {
+        currentTries++;
+        if (!Mathf.Abs(Quaternion.Dot(defaultRotation, transform.localRotation)).ApproxEquals(1f, pinFallThreshold))
+        {
+            Invoke("HidePin", toppleLife);
+        }
+        else
+        {
+            if (currentTries > tries)
+            {
+                CancelToppleCheck();
+            }
+        }
+    }
+
+    protected void HidePin()
+    {
+        gameObject.SetActive(false);
+    }
+}
